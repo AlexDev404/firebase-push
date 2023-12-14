36e9119cb2bf1654f2a3dd8ea303185243654623
AlexDev404 <AlexDev404@users.noreply.github.com>
2023-12-14 10:09:16 -0600

    Switch `onTimeout` to `onMount/useEffect`

--- App.tsx
+++ App.tsx
@@ -1,11 +1,16 @@
+// Import necessary dependencies
 import { Text, SafeAreaView, StyleSheet, Button } from "react-native";
 import Toast from "react-native-toast-message";
 import { initializeApp } from "firebase/app";
 import { onMessage, getToken } from "firebase/messaging";
 import { getMessaging } from "firebase/messaging/sw";
 import { Card } from "react-native-paper";
+import { useEffect } from "react";
+
+// Retrieve subscribed status from local storage
 const subscribed = JSON.parse(localStorage.getItem("subscribed"));
 
+// Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyDJWYR522sr-NEVVUvnAtXbtids2O2KLd0",
   authDomain: "ub-loopback.firebaseapp.com",
@@ -16,68 +21,11 @@ const firebaseConfig = {
   measurementId: "G-MXP4BEY67C",
 };
 
+// Initialize Firebase app and messaging
 const app = initializeApp(firebaseConfig);
 const messaging = getMessaging(app);
 
-// Subscribe to "default" topic
-
-setTimeout(() => {
-  getToken(messaging, {
-    vapidKey:
-      "BGYNclealf3yT28K2-DUH5HJTrCtvqdARU8GGdxyC6yQcFUFGRP9y_QnmCbbi40wCJGhHvhb3zTg7aVRNKHrcjs",
-  })
-    .then((currentToken) => {
-      if (currentToken) {
-        // Send the token to your server and update the UI if necessary
-        if (!subscribed) {
-          fetch("http://localhost:3000/subscribe", {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-              token: currentToken,
-              topic: "default",
-            }),
-          })
-            .then((response) => response.json())
-            .then((data) => {
-              console.log(data);
-              Toast.show({
-                type: "info",
-                text1: "Information",
-                text2: data.message,
-              });
-            })
-            .catch((error) => {
-              Toast.show({
-                type: "error",
-                text1: "Error",
-                text2: error.toString(),
-              });
-            });
-          localStorage.setItem("subscribed", "true");
-        }
-      } else {
-        // Show permission request UI
-        Toast.show({
-          type: "warn",
-          text1:
-            "No registration token available. Request permission to generate one.",
-        });
-        // ...
-      }
-    })
-    .catch((err) => {
-      Toast.show({
-        type: "error",
-        text1: "An error occurred while retrieving token.",
-        text2: err.toString(),
-      });
-      // ...
-    });
-}, 3000);
-
+// Handle incoming messages
 onMessage(messaging, (payload) => {
   console.log("Message received.", payload);
   Toast.show({
@@ -87,7 +35,66 @@ onMessage(messaging, (payload) => {
   });
 });
 
+// App component
 export default function App() {
+  // Subscribe to "default" topic on component mount
+  useEffect(() => {
+    getToken(messaging, {
+      vapidKey:
+        "BGYNclealf3yT28K2-DUH5HJTrCtvqdARU8GGdxyC6yQcFUFGRP9y_QnmCbbi40wCJGhHvhb3zTg7aVRNKHrcjs",
+    })
+      .then((currentToken) => {
+        if (currentToken) {
+          // Send the token to your server and update the UI if necessary
+          if (!subscribed) {
+            fetch("http://localhost:3000/subscribe", {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify({
+                token: currentToken,
+                topic: "default",
+              }),
+            })
+              .then((response) => response.json())
+              .then((data) => {
+                console.log(data);
+                Toast.show({
+                  type: "info",
+                  text1: "Information",
+                  text2: data.message,
+                });
+              })
+              .catch((error) => {
+                Toast.show({
+                  type: "error",
+                  text1: "Error",
+                  text2: error.toString(),
+                });
+              });
+            localStorage.setItem("subscribed", "true");
+          }
+        } else {
+          // Show permission request UI
+          Toast.show({
+            type: "warn",
+            text1:
+              "No registration token available. Request permission to generate one.",
+          });
+          // ...
+        }
+      })
+      .catch((err) => {
+        Toast.show({
+          type: "error",
+          text1: "An error occurred while retrieving token.",
+          text2: err.toString(),
+        });
+        // ...
+      });
+  }, []); // Empty dependency array to run the effect only once
+
   return (
     <SafeAreaView style={styles.container}>
       <Toast />
@@ -109,6 +116,7 @@ export default function App() {
   );
 }
 
+// Styles
 const styles = StyleSheet.create({
   container: {
     flex: 1,
